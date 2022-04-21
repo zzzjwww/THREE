ba7676b1e2c2531c0e25628fa0c44f368504f9ee
Zijian Wang <zw@ZijiandeMBP.cable.rcn.com>
2022-04-20 22:02:26 -0400

    Material

--- src/script.js
+++ src/script.js
@@ -6,6 +6,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  * Txtures
  */
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
 const doorColorTexture = textureLoader.load("/textures/door/color.jpg")
 const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg")
 const doorAmbientOcclusionTexture = textureLoader.load("/textures/door/AmbientOcclusion.jpg")
@@ -16,6 +18,18 @@ const doorRoughnessTexture = textureLoader.load("/textures/door/Roughness.jpg")
 
 const matcapTexture = textureLoader.load("/textures/matcaps/7.png")
 const gradientTexture = textureLoader.load("/textures/gradients/3.jpg")
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
+
+const environmentMapTexture = cubeTextureLoader.load([
+    "./textures/environmentMaps/0/px.jpg",
+    "./textures/environmentMaps/0/nx.jpg",
+    "./textures/environmentMaps/0/py.jpg",
+    "./textures/environmentMaps/0/ny.jpg",
+    "./textures/environmentMaps/0/pz.jpg",
+    "./textures/environmentMaps/0/nz.jpg"
+])
 /**
  * Base
  */
@@ -43,24 +57,81 @@ const scene = new THREE.Scene()
 // const material = new THREE.MeshMatcapMaterial()
 // material.matcap = matcapTexture
 
-const material = new THREE.MeshDepthMaterial()
+// const material = new THREE.MeshDepthMaterial()
+
+// const material = new THREE.MeshLambertMaterial()
+
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 100
+// material.specular = new THREE.Color("red")
+
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+
+// const material = new THREE.MeshStandardMaterial()
+// material.metalness = 0.45
+// material.roughness = 0.45
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.1
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5,0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 1
+material.roughness = 0.2
+material.envMap = environmentMapTexture
+
 
 const sphere = new THREE.Mesh (
-    new THREE.SphereGeometry(0.5,16,16),
+    new THREE.SphereGeometry(0.5,64,64),
     material
 )
 sphere.position.x = -1.5
+
+sphere.geometry.setAttribute("uv2",
+    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array,2)
+)
 const plane = new THREE.Mesh (
-    new THREE.PlaneGeometry(1,1),
+    new THREE.PlaneGeometry(1,1, 100, 100),
     material
 )
+
+plane.geometry.setAttribute("uv2",
+    new THREE.BufferAttribute(plane.geometry.attributes.uv.array,2)
+)
+
 const torus = new THREE.Mesh (
-    new THREE.TorusGeometry(0.3,0.2,16,32),
+    new THREE.TorusGeometry(0.3,0.2,64,128),
     material
 )
 torus.position.x = 1.5
 
+torus.geometry.setAttribute("uv2",
+    new THREE.BufferAttribute(torus.geometry.attributes.uv.array,2)
+)
+
 scene.add(sphere, plane, torus)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+
+scene.add(pointLight)
+
 /**
  * Sizes
  */
