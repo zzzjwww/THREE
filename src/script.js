7b53552088fa8d224d7147f8cd180bcc61e97fed
Zijian Wang <zw@ZijiandeMBP.cable.rcn.com>
2022-04-24 12:38:30 -0400

    Text geometry

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
+import { TextGeometry} from "three/examples/jsm/geometries/TextGeometry.js"
+import { RGBA_ASTC_10x10_Format } from 'three'
+
+
+
 
 /**
  * Txtures
@@ -16,7 +22,7 @@ const doorNormalTexture = textureLoader.load("/textures/door/Normal.jpg")
 const doorMetalnessTexture = textureLoader.load("/textures/door/Metalness.jpg")
 const doorRoughnessTexture = textureLoader.load("/textures/door/Roughness.jpg")
 
-const matcapTexture = textureLoader.load("/textures/matcaps/7.png")
+const matcapTexture = textureLoader.load("/textures/matcaps/1.png")
 const gradientTexture = textureLoader.load("/textures/gradients/3.jpg")
 gradientTexture.minFilter = THREE.NearestFilter
 gradientTexture.magFilter = THREE.NearestFilter
@@ -30,6 +36,60 @@ const environmentMapTexture = cubeTextureLoader.load([
     "./textures/environmentMaps/0/pz.jpg",
     "./textures/environmentMaps/0/nz.jpg"
 ])
+
+/**
+ * fonts
+ */
+
+const fontLoader = new FontLoader()
+fontLoader.load(
+     "/fonts/helvetiker_regular.typeface.json",
+     (font)=>{
+         const textGeometry = new TextGeometry(
+         "ZIJIAN, HELLO!",
+         {
+             font:font,
+             size: 0.5,
+             height:0.2,
+             curveSegment: 5,
+             bevelEnabled: true,
+             bevelThickness: 0.03,
+             bevelSize: 0.02,
+             bevelOffset: 0,
+             bevelSegments: 3
+         }
+         )
+         textGeometry.center()
+         const textMaterial = new THREE.MeshMatcapMaterial()
+         textMaterial.matcap = matcapTexture
+         const text = new THREE.Mesh(textGeometry,textMaterial)
+         scene.add(text)
+
+         const donutGeometry = new THREE.TorusGeometry(0.3,0.2,20,45)
+         const donutMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+         
+         for (let i = 0; i<100; i++) {
+             const donuts = new THREE.Mesh(donutGeometry,donutMaterial)
+             
+             donuts.position.x = (Math.random() - 0.5) * 10
+             donuts.position.y = (Math.random() - 0.5) * 10
+             donuts.position.z = (Math.random() - 0.5) * 10
+        
+             donuts.rotation.x = (Math.random() - 0.5) * Math.PI
+             donuts.rotation.x = (Math.random() - 0.5) * Math.PI
+        
+             const scale = Math.random()
+             donuts.scale.set(scale, scale, scale)
+             scene.add(donuts)
+         }
+     }
+
+     
+ )
+ 
+
+
+
 /**
  * Base
  */
@@ -38,6 +98,10 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Objects
@@ -88,36 +152,38 @@ material.metalness = 1
 material.roughness = 0.2
 material.envMap = environmentMapTexture
 
+/**
+ * Geometry 
+ */
+// const sphere = new THREE.Mesh (
+//     new THREE.SphereGeometry(0.5,64,64),
+//     material
+// )
+// sphere.position.x = -1.5
 
-const sphere = new THREE.Mesh (
-    new THREE.SphereGeometry(0.5,64,64),
-    material
-)
-sphere.position.x = -1.5
+// sphere.geometry.setAttribute("uv2",
+//     new THREE.BufferAttribute(sphere.geometry.attributes.uv.array,2)
+// )
+// const plane = new THREE.Mesh (
+//     new THREE.PlaneGeometry(1,1, 100, 100),
+//     material
+// )
 
-sphere.geometry.setAttribute("uv2",
-    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array,2)
-)
-const plane = new THREE.Mesh (
-    new THREE.PlaneGeometry(1,1, 100, 100),
-    material
-)
+// plane.geometry.setAttribute("uv2",
+//     new THREE.BufferAttribute(plane.geometry.attributes.uv.array,2)
+// )
 
-plane.geometry.setAttribute("uv2",
-    new THREE.BufferAttribute(plane.geometry.attributes.uv.array,2)
-)
+// const torus = new THREE.Mesh (
+//     new THREE.TorusGeometry(0.3,0.2,64,128),
+//     material
+// )
+// torus.position.x = 1.5
 
-const torus = new THREE.Mesh (
-    new THREE.TorusGeometry(0.3,0.2,64,128),
-    material
-)
-torus.position.x = 1.5
+// torus.geometry.setAttribute("uv2",
+//     new THREE.BufferAttribute(torus.geometry.attributes.uv.array,2)
+// )
 
-torus.geometry.setAttribute("uv2",
-    new THREE.BufferAttribute(torus.geometry.attributes.uv.array,2)
-)
-
-scene.add(sphere, plane, torus)
+// scene.add(sphere, plane, torus)
 
 /**
  * Lights
@@ -187,13 +253,13 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
     //Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    plane.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
+    // sphere.rotation.y = 0.1 * elapsedTime
+    // plane.rotation.y = 0.1 * elapsedTime
+    // torus.rotation.y = 0.1 * elapsedTime
 
-    sphere.rotation.x = 0.15 * elapsedTime
-    plane.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
+    // sphere.rotation.x = 0.15 * elapsedTime
+    // plane.rotation.x = 0.15 * elapsedTime
+    // torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
