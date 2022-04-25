533ad77a456292392ca699a916dc8658408de034
Zijian Wang <zw@ZijiandeMBP.cable.rcn.com>
2022-04-25 11:02:24 -0400

    Vercel Ready

--- src/script.js
+++ src/script.js
@@ -22,7 +22,7 @@ const doorNormalTexture = textureLoader.load("/textures/door/Normal.jpg")
 const doorMetalnessTexture = textureLoader.load("/textures/door/Metalness.jpg")
 const doorRoughnessTexture = textureLoader.load("/textures/door/Roughness.jpg")
 
-const matcapTexture = textureLoader.load("/textures/matcaps/7.png")
+const matcapTexture = textureLoader.load("/textures/matcaps/6.png")
 const gradientTexture = textureLoader.load("/textures/gradients/3.jpg")
 gradientTexture.minFilter = THREE.NearestFilter
 gradientTexture.magFilter = THREE.NearestFilter
